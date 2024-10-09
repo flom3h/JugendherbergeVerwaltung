fa565e77a319d29d461e1cf45ffae4b06bed9510
pieles.florian@gmail.com <pieles.florian@gmail.com>
2024-10-09 09:36:11 +0000

    Created Startseite and Database_Server; Edited settings; Deleted Form1

---
+++ client_code/Startseite/__init__.py
@@ -0,0 +1,13 @@
+from ._anvil_designer import StartseiteTemplate
+from anvil import *
+import anvil.server
+
+
+class Startseite(StartseiteTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    self.drop_down_1.items = [("Feldkirch", 0), ("Mordor", 1)]
+    # Any code you write here will run before the form opens.
+    print(anvil.server.call("say_hello", "sauron"))

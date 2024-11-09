d4901a7b7e2ef185b2916b22cf9f3efef3cac3a7
pieles.florian@gmail.com <pieles.florian@gmail.com>
2024-11-09 09:48:51 +0000

    Edited Startseite

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -13,7 +13,6 @@ class Startseite(StartseiteTemplate):
 
     # [("Feldkirch", 0), ("Mordor", 1)]
     # Any code you write here will run before the form opens.
-    print(anvil.server.call("say_hello", "sauron"))
     print(anvil.server.call("get_jugendherbergen"))
     self.drop_down_1.items = [(name, JID) for name, JID in anvil.server.call("get_jugendherbergen", "name, JID")]
     if self.drop_down_1.items:
